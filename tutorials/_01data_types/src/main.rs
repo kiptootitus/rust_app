58ca2fa922f90d6f8041c2d8327f5d10935ae9a1
naruto <kiptootitus75@gmail.com>
2025-05-04 21:00:57 +0300

    bbb

--- tutorials/_01data_types/src/main.rs
+++ tutorials/_01data_types/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(warnings)]
+
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
+use rand::Rng;
+use std::cmp::Ordering;
+
+#[tokio::main]
+
+async fn main() {
+    println!("MAX u32: {}", u32::MAX);
+    println!("MAX u64: {}", u64::MAX);
+    println!("MAX i32: {}", i32::MAX);
+    println!("MAX f32: {}", f32::MAX);
+
+
+
+    // doubles quotes create a string while single create a character
+    let _my_char = 'A';
+    
+
 }
